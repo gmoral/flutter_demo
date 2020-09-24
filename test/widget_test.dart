c86da3e3b06110f4d42c197baec7715436ce7434
Guillermo Moral <willy.moral@gmail.com>
2020-09-24 19:49:58 -0300

    Add a widget test
    This is an example of how to check a widget using the framework async_redux: ^2.7.2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutterdemo/main.dart';
+import '../../flutter_demo/lib/client/sign_in/sign_in_page.dart';
 
 void main() {
-  testWidgets('Demo', (WidgetTester tester) async {
-    // TODO
+  TestWidgetsFlutterBinding.ensureInitialized();
+  const Duration delayedDuration = Duration(milliseconds: 50);
+
+  testWidgets('Test Sign in', (WidgetTester tester) async {
+
+    //Given
+    bool waiting = false;
+    VoidCallback loginPage;
+    VoidCallback checkAuthState;
+    VoidCallback signInWithPin;
+
+    //When
+    await tester
+        .pumpWidget(MaterialApp(home: SignInPage(waiting: waiting, loginPage: loginPage, checkAuthState: checkAuthState, signInWithPin: signInWithPin, authState: null)));
+
+    await tester.pump();
+
+    //Them
+    expect(find.byKey(SignInPage.signInButtonKey), findsOneWidget);
+    expect(find.byKey(SignInPage.signInDetailKey), findsOneWidget);
+    expect(find.byKey(SignInPage.signInTextKey), findsOneWidget);
+
   });
 }
