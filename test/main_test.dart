37ba81558f5f9cffeb4f7bb901f28d09c4195e31
Guillermo Moral <gmoral@celltrak.com>
2020-09-25 12:24:54 -0300

    Update main test

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,48 +1,50 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:flutterdemo/business/app_state_store.dart';
 import 'package:flutterdemo/business/login/actions/LoginAction.dart';
-import 'package:flutterdemo/services/storage/actions/InsertUserAction.dart';
-import 'package:flutterdemo/services/service_locator.dart';
 import 'package:flutterdemo/business/login/models/login_state.dart';
-import 'package:flutterdemo/services/storage/model/user.dart';
+import 'package:flutterdemo/services/authService/fakeAuthService.dart';
+import 'package:flutterdemo/business/auth/models/auth_state.dart';
+import 'package:flutterdemo/business/auth/models/auth.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutterdemo/business/login/models/LoginResponse.dart';
+
+class AuthServiceMock extends Mock implements FakeAuthService {}
 
 void main() {
   test('Login', () async {
-    states = [];
+    // Given
+    final authService = AuthServiceMock();
+    final authStateMock = AuthState(
+        pageState: AuthPageState.Start,
+        auth: Auth(pinEntry: '', pinConfirm: ''));
+    final loginStateMock = LoginState(pageState: LoginPageState.Initial);
 
-    var storeTester = StoreTester<AppState>(initialState: AppState.initial());
+    final appStateMock = AppState(
+        waiting: false,
+        isLogin: false,
+        isUnitTesting: true,
+        loginState: loginStateMock,
+        authState: authStateMock);
 
-    /// Set isUnitTesting: true when run unit testing.
-    storeTester.state.copyWith(isUnitTesting: true);
+    var storeTester = StoreTester<AppState>(initialState: appStateMock);
 
-    setupLocator();
+    LoginResponse loginResponse =
+        LoginResponse(success: true, userId: 1, message: '');
 
-    expect(storeTester.state.isLogin, false);
+    when(authService.login(username: 'test1@test.com', password: 'test1'))
+        .thenAnswer((_) async => loginResponse);
 
-    storeTester
-        .dispatch(LoginAction(username: 'test1@test.com', password: 'test1'));
-
+    // When
+    storeTester.dispatch(LoginAction(
+        username: 'test1@test.com',
+        password: 'test1',
+        authService: authService));
     TestInfo<AppState> info = await storeTester.waitUntil(LoginAction);
 
-    expect(info.state.waiting, false);
+    // Then
+    expect(info.state.isLogin, true);
     expect(info.state.loginState.pageState, LoginPageState.NextToHomePage);
-  });
-
-  test('InsertUser', () async {
-    states = [];
-
-    var storeTester = StoreTester<AppState>(initialState: AppState.initial());
-
-    /// Set isUnitTesting: true when run unit testing.
-    storeTester.state.copyWith(isUnitTesting: true);
-
-    setupLocator();
-
-    User aUser = User(username: 'test1', isActive: true);
-    storeTester.dispatch(InsertUserAction(user: aUser));
-
-    TestInfo<AppState> info = await storeTester.waitUntil(InsertUserAction);
-    expect(info.state.waiting, false);
   });
 }
